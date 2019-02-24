ad906734b15095ab5cf21098b6a74645d74214d5
Takuro Wada <taxpon@gmail.com>
2019-02-24 09:22:29 +0900

    Add script for version management

---
+++ pytidy/__init__.py
@@ -0,0 +1,11 @@
+__version__ = "0.2.0"
+
+__title__ = "pytidy"
+__description__ = "Dependency Injection library based on type hints"
+__url__ = "https://github.com/taxpon/pytidy"
+
+__author__ = "Takuro Wada"
+__email__ = "taxpon@gmail.com"
+
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2019 Takuro Wada"
